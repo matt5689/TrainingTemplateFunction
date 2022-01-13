81d4e7dc0e11c0def1303ce488d5569daf9e8acf
Matthew <matt.cavallo56@gmail.com>
2022-01-13 09:56:47 -0600

    First

--- TemplateFunctionApp/ServiceBusQueueTrigger.cs
+++ TemplateFunctionApp/ServiceBusQueueTrigger.cs
@@ -1,16 +1,22 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace TemplateFunctionApp
 {
-    public static class ServiceBusQueueTrigger
+    public class ServiceBusQueueTrigger
     {
         [FunctionName("ServiceBusQueueTrigger")]
-        public static void Run([ServiceBusTrigger("trainingservbus5601queue", Connection = "ServiceBusConnectionString")]string myQueueItem, ILogger log)
+        public async Task Run([ServiceBusTrigger("trainingservbus5601queue", Connection = "ServiceBusConnectionString")]string myQueueItem,
+            [ServiceBus("trainingservbus5601queue", Connection = "ServiceBusConnectionString")] IAsyncCollector<string> serviceBusReturn,
+            ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+
+            await serviceBusReturn.AddAsync("Item1");
+            await serviceBusReturn.AddAsync("Item2");
         }
     }
 }
